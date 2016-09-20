d7b7a256cae95c8d0cfe3c4c99c0d9600cd69968
Tomasz Wylężek <Tomasz.Wylezek@jcommerce.pl>
2016-09-20 12:01:42 +0200

    next version

--- routes/book.js
+++ routes/book.js
@@ -1,22 +1,22 @@
 var express = require('express');
-var request = require('request');
+var goodGuy = require('good-guy-http')({maxRetries: 3});
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'hahahhah' });
+    var body = {
+        title: 'Nie ma',
+        subtitle: 'ksionrzki',
+        imageLinks: {
+            thumbnail: 'http://searchengineland.com/figz/wp-content/seloads/2015/10/google-panda-name3-ss-1920-800x450.jpg'
+        }
+    };
+    res.render('book', { body: body });
 });
 router.get('/:isbn', function (req, res, next) {
-    request('https://book-catalog-proxy-1.herokuapp.com/book?isbn=' + req.params.isbn, function (error, response, body) {
-        body = JSON.parse(body);
-        if (error || !body.items) {
-            var err = new Error('Not Found');
-            err.status = 404;
-            next(err);
-        } else {
-            res.render('book', { body: body.items[0].volumeInfo });
-        }
-    });
+    goodGuy(`https://book-catalog-proxy-1.herokuapp.com/book?isbn=${req.params.isbn}`).then(function (response) {
+        var body = JSON.parse(response.body);
+        res.render('book', { body: body.items[0].volumeInfo });
+    }).catch(next);
 });
 
 module.exports = router;
