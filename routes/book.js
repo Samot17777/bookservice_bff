1677c14613e9679641b924b40cbccecc38da4d56
Tomasz Wylężek <Tomasz.Wylezek@jcommerce.pl>
2016-09-20 13:42:04 +0200

    addedd json

--- routes/book.js
+++ routes/book.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var goodGuy = require('good-guy-http')({maxRetries: 3});
+var goodGuy = require('good-guy-http')({ maxRetries: 3 });
 var router = express.Router();
 var jp = require('jsonpath');
 /* GET home page. */
@@ -7,21 +7,28 @@ router.get('/', function (req, res, next) {
     var body = {
         title: 'Nie ma',
         subtitle: 'ksionrzki',
-        imageLinks: {
-            thumbnail: 'http://searchengineland.com/figz/wp-content/seloads/2015/10/google-panda-name3-ss-1920-800x450.jpg'
-        }
+        cover: 'http://searchengineland.com/figz/wp-content/seloads/2015/10/google-panda-name3-ss-1920-800x450.jpg',  
+        layout: 'layout_file'
     };
-    res.render('book', { body: body });
+
+res.render('book', { body: body });
 });
 router.get('/:isbn', function (req, res, next) {
+    req.esiOptions = {
+    headers: {
+      Accept: 'text/html'
+    }
+  };
     goodGuy(`https://book-catalog-proxy-3.herokuapp.com/book?isbn=${req.params.isbn}`).then(function (response) {
         response = JSON.parse(response.body);
-            var title = jp.query(response, '$..title');
-            var subtitle = jp.query(response, '$..subtitle');
-            var cover = jp.query(response, '$..thumbnail');
-            res.render('book', { title,subtitle,cover , partials: {
+        var title = jp.query(response, '$..title');
+        var subtitle = jp.query(response, '$..subtitle');
+        var cover = jp.query(response, '$..thumbnail');
+        res.render('book', {
+            title, subtitle,isbn:req.params.isbn, cover, partials: {
                 layout: 'layout_file'
-            }});
+            }
+        });
 
     }).catch(next);
 });
